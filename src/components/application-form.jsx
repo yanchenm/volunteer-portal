b6c45016ae1065d22a3b5dd78fdbc70f6f61887a
Justin Goping <jgoping@gmail.com>
2019-11-15 16:44:03 -0500

    Create basis for application form

--- src/components/application-form.jsx
+++ src/components/application-form.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
 
-function ApplicationForm(props) {
-  return (
-    <h1>Form goes here</h1>
-  )
-}
+const Wrapper = styled.div`
+  margin: 10px 20%;
+`;
 
-export default ApplicationForm
+const ApplicationForm = () => (
+  <Wrapper>
+    <Form>
+      <Form.Field>
+        <label htmlFor="application-form">
+          First Name
+          <input placeholder="First Name" />
+        </label>
+      </Form.Field>
+      <Form.Field>
+        <label htmlFor="application-form">
+          Last Name
+          <input placeholder="Last Name" />
+        </label>
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label="I agree to the Terms and Conditions" />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
+  </Wrapper>
+);
+
+export default ApplicationForm;
