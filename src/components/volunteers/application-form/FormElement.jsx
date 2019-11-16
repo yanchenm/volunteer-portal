5490e999fc34b462c81567e92a0b79850e922e96
Justin Goping <jgoping@gmail.com>
2019-11-16 11:04:06 -0500

    Create FormElement component

---
+++ src/components/volunteers/application-form/FormElement.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-console */
+import React from 'react';
+import { Input } from 'antd';
+import PropTypes from 'prop-types';
+
+import 'antd/dist/antd.css';
+
+export const FormElement = props => {
+  const { errors, fieldName, handleBlur, handleChange, label, touched, values } = props;
+  return (
+    <div className="form-group">
+      <label htmlFor={fieldName}>{label}</label>
+      <Input
+        onChange={handleChange}
+        name={fieldName}
+        value={values[fieldName]}
+        onBlur={handleBlur}
+      />
+      {errors[fieldName] && touched[fieldName] && (
+        <div style={{ color: 'red' }}>{errors[fieldName]}</div>
+      )}
+    </div>
+  );
+};
+
+FormElement.propTypes = {
+  errors: PropTypes.func.isRequired,
+  fieldName: PropTypes.string.isRequired,
+  handleBlur: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  touched: PropTypes.func.isRequired,
+  values: PropTypes.func.isRequired,
+};
+
+export default FormElement;
