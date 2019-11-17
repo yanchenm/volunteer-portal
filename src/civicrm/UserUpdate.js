488c3200bef8a802b378691f4090e41786439508
Nitin Sachdeo <nitinsachdeo@gmail.com>
2019-11-17 08:43:46 -0500

    Created api calls for updating volunteer state

---
+++ src/civicrm/UserUpdate.js
@@ -0,0 +1,24 @@
+const baseUrl =
+  'https://cors-anywhere.herokuapp.com/' +
+  'http://99.79.69.43/web/libraries/civicrm/extern/rest.php' +
+  '?key=addf0fd097b87e5364a74c07b4ef4e3e&api_key=drupal';
+
+const volunteerInterviewed = contactId => {
+  const requestType = 'entity=Contact&action=create';
+  const body = `json={"id":"${contactId}"` + `,"custom_45":"true"` + `}`;
+
+  return fetch(`${baseUrl}&${requestType}&${body}`, { method: 'POST' }).then(response => {
+    return response.json();
+  });
+};
+
+const volunteerApproved = contactId => {
+  const requestType = 'entity=Contact&action=create';
+  const body = `json={"id":"${contactId}"` + `,"custom_36":"true"` + `}`;
+
+  return fetch(`${baseUrl}&${requestType}&${body}`, { method: 'POST' }).then(response => {
+    return response.json();
+  });
+};
+
+export { volunteerInterviewed, volunteerApproved };
