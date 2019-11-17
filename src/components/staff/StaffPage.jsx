0dbddd071e092b2db5828b67bc579eea913edbd1
Justin Goping <jgoping@gmail.com>
2019-11-17 01:21:21 -0500

    Add modals to staff page so staff can see more about volunteers and accept them

--- src/components/staff/StaffPage.jsx
+++ src/components/staff/StaffPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { List, Card } from 'antd';
+import { List, Card, Modal, Button } from 'antd';
+
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -24,34 +25,91 @@ const fakePeopleToReview = [
   },
 ];
 
-const StaffPage = () => {
-  return (
-    <Wrapper>
-      <h1>Staff Dashboard</h1>
-      Volunteers to approve:
-      <List
-        grid={{
-          gutter: 16,
-          xs: 1,
-          sm: 1,
-          md: 2,
-          lg: 2,
-          xl: 3,
-          xxl: 3,
-        }}
-        dataSource={fakePeopleToReview}
-        renderItem={volunteer => (
-          <List.Item>
-            <Card title={volunteer.name}>
-              {volunteer.email}
-              <br />
-              {volunteer.location}
-            </Card>
-          </List.Item>
-        )}
-      />
-    </Wrapper>
-  );
-};
+class StaffPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: false,
+      currentUser: null,
+    };
+  }
+
+  showModal = index => {
+    this.setState({
+      visible: true,
+      currentUser: fakePeopleToReview[index],
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  acceptVolunteer = () => {
+    // Post to flip accepted flag, remove from list
+  };
+
+  render = () => {
+    const { visible, currentUser } = this.state;
+    return (
+      <Wrapper>
+        <h1>Staff Dashboard</h1>
+        Volunteers to approve:
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 1,
+            md: 2,
+            lg: 2,
+            xl: 3,
+            xxl: 3,
+          }}
+          dataSource={fakePeopleToReview}
+          renderItem={(volunteer, index) => (
+            <List.Item>
+              <Card title={volunteer.name}>
+                {volunteer.email}
+                <br />
+                {volunteer.location}
+                <br />
+                <Button
+                  id={index}
+                  type="primary"
+                  onClick={event => {
+                    this.showModal(parseInt(event.target.id, 10));
+                  }}
+                >
+                  Open Modal
+                </Button>
+              </Card>
+            </List.Item>
+          )}
+        />
+        <Modal
+          title="Volunteer Modal"
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>{currentUser ? currentUser.name : ''}</p>
+          <p>This will be content about the user.</p>
+          <Button type="primary" onClick={this.acceptVolunteer()}>
+            Accept
+          </Button>
+        </Modal>
+      </Wrapper>
+    );
+  };
+}
 
 export default StaffPage;
