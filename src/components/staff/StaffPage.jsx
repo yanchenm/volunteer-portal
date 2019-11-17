99d478edbf6465e61dfb2bb62c0c0fa087221d63
Justin Goping <jgoping@gmail.com>
2019-11-17 11:45:36 -0500

    Make staff page connect to CIVI

--- src/components/staff/StaffPage.jsx
+++ src/components/staff/StaffPage.jsx
@@ -1,29 +1,15 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-console */
 import React from 'react';
-import { List, Card, Modal, Button } from 'antd';
-
+import { Descriptions, List, Card, Modal, Button } from 'antd';
 import styled from 'styled-components';
+import { listVolunteers } from '../../civicrm/QueryUsers';
+import { volunteerInterviewed, volunteerApproved } from '../../civicrm/UserUpdate';
 
 const Wrapper = styled.div`
   margin: 10px 20%;
 `;
-
-const fakePeopleToReview = [
-  {
-    name: 'User1',
-    email: 'user1@gmail.com',
-    location: 'Scarborough',
-  },
-  {
-    name: 'User2',
-    email: 'user2@gmail.com',
-    location: 'Toronto',
-  },
-  {
-    name: 'User3',
-    email: 'user3@gmail.com',
-    location: 'Thunder Bay',
-  },
-];
 
 class StaffPage extends React.Component {
   constructor(props) {
@@ -32,13 +18,34 @@ class StaffPage extends React.Component {
     this.state = {
       visible: false,
       currentUser: null,
+      interviewList: [],
+      approveList: [],
     };
   }
 
-  showModal = index => {
+  componentDidMount() {
+    let interviewList;
+    let approveList;
+    listVolunteers(false, false).then(res => {
+      interviewList = Object.values(res.values);
+      this.setState({
+        interviewList,
+      });
+    });
+    listVolunteers(true, false).then(res => {
+      approveList = Object.values(res.values);
+      this.setState({
+        approveList,
+      });
+    });
+  }
+
+  showModal = (index, inInterviewList) => {
     this.setState({
       visible: true,
-      currentUser: fakePeopleToReview[index],
+      currentUser: inInterviewList
+        ? this.state.interviewList[index]
+        : this.state.approveList[index],
     });
   };
 
@@ -54,15 +61,79 @@ class StaffPage extends React.Component {
     });
   };
 
-  acceptVolunteer = () => {
+  interviewVolunteer = () => {
+    volunteerInterviewed(12);
+  };
+
+  approveVolunteer = () => {
     // Post to flip accepted flag, remove from list
+    volunteerApproved(12);
   };
 
   render = () => {
-    const { visible, currentUser } = this.state;
+    const ModalDashboard = currentUser => {
+      console.log(currentUser);
+      if (currentUser) {
+        return (
+          <div>
+            <p>asodijsdoijsdoifj</p>
+            <Descriptions size="small" column={2} bordered>
+              {Object.keys(currentUser.currentUser).map(key => {
+                if (
+                  currentUser.currentUser[key] !== '' &&
+                  key !== 'firstName' &&
+                  key !== 'lastName'
+                ) {
+                  return (
+                    <Descriptions.Item label={key}>
+                      {currentUser.currentUser[key]}
+                    </Descriptions.Item>
+                  );
+                }
+                return null;
+              })}
+            </Descriptions>
+          </div>
+        );
+      }
+      return <div />;
+    };
+    const { visible, currentUser, interviewList, approveList } = this.state;
     return (
       <Wrapper>
         <h1>Staff Dashboard</h1>
+        Volunteers to interview:
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 1,
+            md: 2,
+            lg: 2,
+            xl: 3,
+            xxl: 3,
+          }}
+          dataSource={interviewList}
+          renderItem={(volunteer, index) => (
+            <List.Item>
+              <Card title={volunteer.firstName}>
+                {volunteer.email}
+                <br />
+                {volunteer.location}
+                <br />
+                <Button
+                  id={index}
+                  type="primary"
+                  onClick={event => {
+                    this.showModal(parseInt(event.target.id, 10), true);
+                  }}
+                >
+                  Open Modal
+                </Button>
+              </Card>
+            </List.Item>
+          )}
+        />
         Volunteers to approve:
         <List
           grid={{
@@ -74,7 +145,7 @@ class StaffPage extends React.Component {
             xl: 3,
             xxl: 3,
           }}
-          dataSource={fakePeopleToReview}
+          dataSource={approveList}
           renderItem={(volunteer, index) => (
             <List.Item>
               <Card title={volunteer.name}>
@@ -86,7 +157,7 @@ class StaffPage extends React.Component {
                   id={index}
                   type="primary"
                   onClick={event => {
-                    this.showModal(parseInt(event.target.id, 10));
+                    this.showModal(parseInt(event.target.id, 10), false);
                   }}
                 >
                   Open Modal
@@ -100,10 +171,12 @@ class StaffPage extends React.Component {
           visible={visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          width={1000}
         >
-          <p>{currentUser ? currentUser.name : ''}</p>
-          <p>This will be content about the user.</p>
-          <Button type="primary" onClick={this.acceptVolunteer()}>
+          <p>{currentUser ? `${currentUser.first_name} ${currentUser.last_name}` : ''}</p>
+          {/* <p>This will be content about the user.</p> */}
+          <ModalDashboard currentUser={currentUser} />
+          <Button type="primary" onClick={this.approveVolunteer()}>
             Accept
           </Button>
         </Modal>
